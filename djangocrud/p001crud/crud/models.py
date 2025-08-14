c520ebea8ff3928c51bdd68fc861214c2fff54db
MaSalcedo10 <masg1606@hotmail.com>
2025-08-13 22:12:56 -0500

    Ejercicio CRUD Django

---
+++ djangocrud/p001crud/crud/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+
+    class Meta:
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.title
